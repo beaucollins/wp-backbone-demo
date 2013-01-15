4ab55edfecff8f562623df0f041aae6f18e82599
Beau Collins <beaucollins@gmail.com>
2013-01-15 12:20:31 -0800

    Adding a meta box to display UI

--- functions.php
+++ functions.php
@@ -25,6 +25,31 @@ class Demo {
 		}		
 	}
 
+	/**
+	 * Displays a meta box for rendering our map UI
+	 *
+	 * @param string $post WordPress post object
+	 * @return void
+	 */
+	function meta_content( $post ){
+		// retrive post meta using our key constant
+		$lat_lng = get_post_meta( $post->ID, self::POST_META_KEY, true );
+		// render some HTML that we can use to display our UI
+		?>
+		<div id="demo-map">
+			<input type="hidden" name="<?php echo self::POST_META_KEY; ?>" value=""></input>
+		</div>
+		<script type="text/javascript">
+		if (window.demo) {
+			jQuery(document).ready(function(){
+				// initializes demo.js
+				window.demo();
+			});
+		};
+		</script>
+		<?php
+	}
+
 }
 
 add_action( 'admin_init', array( new Demo, 'admin_init' ) );
